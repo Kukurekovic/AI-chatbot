7778b84673769002f8ece1e5070b5f48d585d676
Kukurekovic <kukurekovicmarko@gmail.com>
2025-06-02 16:25:36 +0200

    env production

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -18,7 +18,7 @@ const ChatComponent: React.FunctionComponent = () => {
   return (
     <div className="flex flex-col h-[80vh] bg-white">
       <h2 className="p-4 font-semibold text-lg text-center bg-blue-100 flex text-blue-800 justify-center items-center gap-2">
-        React + Gemini AI Chatbot <LuBot size={25} />
+        React AI Chatbot <LuBot size={25} />
       </h2>
       <div ref={ref} className="flex-1 overflow-y-auto p-4 space-y-2">
         {messages.map((msg, index) => (
